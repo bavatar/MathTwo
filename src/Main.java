48990c4beabbd55d6a8f03e666e4c7cf20b329b8
bavatar <janderja@comcast.net>
2019-09-04 11:49:27 -0400

    Math Two Exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//Math Two Exercise
+//        Write an application that assigns 2 to a variable
+//        named mynumber and 1.7938
+//        to a variable namedmyothernumber, and then prints out the values to the console.
+
+public class Main {
+    public static void main(String[] args){
+        int myNumber = 2;
+        float namedMyOtherNumber;
+
+        namedMyOtherNumber= 1.7938F;
+        System.out.println("My Number: " + myNumber);
+        System.out.println("My Other Number: " + namedMyOtherNumber);
+    }
+}
